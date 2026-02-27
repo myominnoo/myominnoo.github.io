16cc6ec6a6fdc3c1767267a4502e5f742ebf688f
Myo Minn Oo <dr.myominnoo@gmail.com>
2026-02-27 08:11:59 -0600

    added renv

---
+++ dependencies.R
@@ -0,0 +1,14 @@
+
+# install.packages("pak")
+pak::pak(c(
+  "dplyr",
+  "ggprism",
+  'haven',
+  'here',
+  'kableExtra',
+  'mStats',
+  'rmarkdown',
+  'tidyverse',
+  "downlit", #code linking
+  "xml2" #code linking
+))
